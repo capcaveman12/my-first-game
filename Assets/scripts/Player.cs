89c86da459adc20b403a9435813562f3555c79dc
adam mcgee <adammcgee1990@outlook.com>
2024-05-22 14:26:52 -0700

    created player and player mat plus added behavior script

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take current position set to 0 at start of the game//
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+
+    }
+}
