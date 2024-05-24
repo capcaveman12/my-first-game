768a1a067dcd96d259e2d60a506c60fc835d6370
adam mcgee <adammcgee1990@outlook.com>
2024-05-23 17:00:47 -0700

    Set up player bounds

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -21,6 +21,21 @@ public class Player : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+        if (transform.position.x >= 11)
+        {
+            transform.position = new Vector3(-11f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11f)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
     }
 }
