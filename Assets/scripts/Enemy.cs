2efc40ad752c2f2fee13ede5e08a4033a63f2953
adam mcgee <adammcgee1990@outlook.com>
2024-05-29 11:22:42 -0700

    Added enemy prefab and enemy behaviour

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 1.0f;
+    void Start()
+    {
+        // enemy needs to move down and respawn at random x axis//
+        transform.position = new Vector3(Random.Range(-9.3f, 9.3f), Random.Range(0, 6.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _position = transform.position.y;
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (_position <= -3.8f)
+        {
+            transform.position = new Vector3(Random.Range(-9.3f, 9.3f), 6.0f, 0);
+        }
+    }
+}
